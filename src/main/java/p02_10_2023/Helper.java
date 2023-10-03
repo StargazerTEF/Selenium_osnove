4f0faa05a253157a62ab9a47f00515bfd51dd2f9
Marko Peric <markoperic986@gmail.com>
2023-10-03 17:23:38 +0200

    Add helper class with methods for download, screenshot and checking status codes

---
+++ src/main/java/p02_10_2023/Helper.java
@@ -0,0 +1,51 @@
+package p02_10_2023;
+
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Helper {
+
+    public static int getHTTPResponseStatusCode(String u) throws IOException {
+        URL url = new URL(u);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        return http.getResponseCode();
+    }
+
+    public static void takeScreenshot(WebDriver driver, String downloadPath) throws IOException {
+        File f = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Files.copy(f, new File(downloadPath));
+    }
+
+    public static void downloadUsingStream(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count = 0;
+        while ((count = bis.read(buffer, 0, 1024)) != -1) {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
+
+    public static void downloadUsingNIO(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+    }
+}
