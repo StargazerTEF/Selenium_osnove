0cede41277a0bf2dedd1ca83ad07ae44d9e52ec3
Marko Peric <markoperic986@gmail.com>
2023-10-10 12:51:34 +0200

    Add Retry analyzers for Bootsnipp and Swag lab tests

---
+++ src/main/java/retry/BootsnippRetry.java
@@ -0,0 +1,19 @@
+package retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class BootsnippRetry implements IRetryAnalyzer {
+    private int count = 0;
+    private int maxCount = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxCount) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
+
